d9aa409c7dcacba50004de432506842ea79ae8e6
Mateusz Derks <mateusz.derks@toptal.com>
2020-02-26 19:00:37 +0100

    Add memoization sample

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function Component({ title, onButtonClick }) {
+  return <div onClick={onButtonClick} style={{ marginTop: '12px' }}>
+    <div>{title}</div>
+    <div>now={Date.now()}</div>
+  </div>
+}
+
+const MemoizedComponent = React.memo(Component)
+
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const memoizedCallback = React.useCallback(() => {
+    console.log('Component clicked')
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <span>Increase counter </span>
+        <button onClick={() => setCounter(counter + 1)}>Click</button>
+      </div>
+      <h1>Counter {counter}</h1>
+
+      <div>
+        <Component title='Regular component, inline callback' onButtonClick={() => console.log('Component clicked')}/>
+        <MemoizedComponent title='Memoized component, inline callback'  onButtonClick={() => console.log('Component clicked')} />
+
+        <Component title='Regular component, memoized callback' onButtonClick={memoizedCallback} />
+        <MemoizedComponent title='Memoized component, memoized callback'  onButtonClick={memoizedCallback} />
+      </div>
     </div>
   );
 }
